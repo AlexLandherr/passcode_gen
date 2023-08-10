538ee10dc4f2149a4a34471b2d9a58d2726ba98e
AlexLandherr <alex.landherr@gmail.com>
2023-08-10 13:31:54 +0000

    Init commit.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+/*Gör en funktion som skapar en slumpmässig fyrsiffrig portkod med följande villkor:
+- Koden får börja med vilken siffra som helst, inklusive 0, men inte med 1
+- Ingen siffra i koden får finnas mer än två gånger. 2234 är ok men inte 2232.*/
+
+int main() {
+    
+    return 0;
+}
